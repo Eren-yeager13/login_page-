df036de7440d22ee6d18fbf9854713f7c1731e83
Anass-Rwichi <127263095+Eren-yeager13@users.noreply.github.com>
2023-05-25 12:27:21 +0100

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE login_page (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(255),
+    lastName VARCHAR(255),
+    email VARCHAR(255),
+    phoneNumber INT,
+    Password VARCHAR(255)
+);
